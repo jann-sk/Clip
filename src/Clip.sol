e85312a79fb6cbde853b49a5bfb897ff91e0d3b9
Janani S K <jananishivk97@gmail.com>
2023-08-20 02:26:55 +0530

    Updated implementation and test cases

--- src/Clip.sol
+++ src/Clip.sol
@@ -1,29 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {ETHToken} from "./ETHToken.sol";
 import {USDCToken} from "./USDCToken.sol";
 import {Vault} from "./Vault.sol";
 
 contract Clip {
-    string public name = "Clip Token Farm";
     address public owner;
     USDCToken public usdcToken;
-    ETHToken public ethToken;
-    Vault public vault;
 
-    uint public s_lastTimeStamp;
-    uint private i_interval = 604800; // 1 week
-    uint private weekly_rewards = 1000 ether;
+    uint public i_interval = 604800; // 1 week
+    uint public constant WEEKLY_REWARDS = 1000 ether;
+    uint public constant TREASURY_AMOUNT = 5000 ether;
 
     event Clip__WeeklyRewardsReleased();
 
-    constructor(USDCToken _usdcToken, ETHToken _ethToken) {
-        usdcToken = _usdcToken;
-        ethToken = _ethToken;
+    constructor() {
+        usdcToken = new USDCToken(TREASURY_AMOUNT);
         owner = msg.sender;
-        s_lastTimeStamp = block.timestamp;
-        vault = new Vault(_ethToken, _usdcToken);
     }
 
     modifier onlyClipOwner() {
@@ -32,43 +25,16 @@ contract Clip {
     }
 
     // Releases rewards to Vault
-    // Can be configured using Chainlink Keepers - time based trigger
-    function releaseRewards() public onlyClipOwner {
+    function releaseRewards(Vault _vault) public onlyClipOwner {
         require(
-            (block.timestamp - s_lastTimeStamp) > i_interval,
+            (block.timestamp - _vault.s_lastTimeStamp() > i_interval),
             "Time has not passed for releasing rewards!"
         );
 
-        usdcToken.transfer(address(vault), weekly_rewards);
+        _vault.setLastTimeStampOfRewards(block.timestamp);
 
+        usdcToken.transfer(address(_vault), WEEKLY_REWARDS);
+        _vault.distributeRewards();
         emit Clip__WeeklyRewardsReleased();
-    }
-
-    function getVault() public view returns (Vault) {
-        return vault;
-    }
-
-    function getClipETHBalance() public view returns (uint) {
-        return ethToken.balanceOf(address(this));
-    }
-
-    function getClipUSDCBalance() public view returns (uint) {
-        return usdcToken.balanceOf(address(this));
-    }
-
-    function getEthToken() public view returns (ETHToken) {
-        return ethToken;
-    }
-
-    function getUsdcToken() public view returns (USDCToken) {
-        return usdcToken;
-    }
-
-    function getClipOwner() public view returns (address) {
-        return owner;
-    }
-
-    function getInterval() public view returns (uint) {
-        return i_interval;
     }
 }
--- src/Vault.sol
+++ src/Vault.sol
@@ -1,88 +1,82 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {ETHToken} from "./ETHToken.sol";
 import {USDCToken} from "./USDCToken.sol";
 
 contract Vault {
-    string public name = "Vault";
-    address public owner;
-    ETHToken public ethToken;
     USDCToken public usdcToken;
-
-    enum VaultState {
-        OPEN,
-        CALCULATING
-    }
 
     address[] public stakers;
     uint public totalStackingBalance;
     mapping(address => uint) public stakingBalance;
     mapping(address => uint) public rewardAmount;
     mapping(address => bool) public hasStaked;
-    mapping(address => bool) public isStaking;
-    uint public immutable stakeInterval = 604800; // 1 week
-    uint private s_lastTimeStamp;
+
+    uint public s_lastTimeStamp;
     uint private stakersCountPerWeek;
     uint public constant MAX_STAKERS_PER_WEEK = 20;
     uint public constant MAX_STAKE_AMOUNT = 20 ether;
-    VaultState public s_vaultState;
 
-    constructor(ETHToken _ethToken, USDCToken _usdcToken) {
-        ethToken = _ethToken;
+    event UnstakedAndRewardsTransferred(
+        address indexed staker,
+        uint indexed ethBalance,
+        uint indexed rewardAmount
+    );
+
+    constructor(USDCToken _usdcToken) {
         usdcToken = _usdcToken;
         s_lastTimeStamp = block.timestamp;
-        s_vaultState = VaultState.OPEN;
         stakersCountPerWeek = 0;
     }
 
-    function depositEth(uint _amount) public {
-        require(s_vaultState == VaultState.OPEN, "Vault is currently closed!!");
-        require(_amount > 0, "amount cannot be 0");
-        require(
-            _amount <= MAX_STAKE_AMOUNT,
-            "Amount should be less than or equal to 20 ether"
-        );
-
-        if (stakersCountPerWeek == MAX_STAKERS_PER_WEEK) {
+    function depositEth() public payable {
+        if (msg.value < 0) {
+            revert("amount cannot be 0");
+        }
+        if (msg.value > MAX_STAKE_AMOUNT) {
+            revert("Amount should be less than or equal to 20 ether");
+        }
+        if (stakersCountPerWeek >= MAX_STAKERS_PER_WEEK) {
             revert("Stakers allowed per week exceeds");
         }
 
-        // Trasnfer ETH tokens to this contract for staking
-        ethToken.transferFrom(msg.sender, address(this), _amount);
-
-        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
-        totalStackingBalance += _amount;
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + msg.value;
+        totalStackingBalance += msg.value;
 
         if (!hasStaked[msg.sender]) {
             stakers.push(msg.sender);
         }
 
-        isStaking[msg.sender] = true;
         hasStaked[msg.sender] = true;
         stakersCountPerWeek += 1;
     }
 
     // Unstaking Tokens (Withdraw)
-    function claimRewards() public {
+    function claimRewards() public payable {
         // Fetch staking balance
         uint balance = stakingBalance[msg.sender];
         uint rewardAmountOfSender = rewardAmount[msg.sender];
 
-        require(balance > 0, "No Staking balance available for the user");
-
-        ethToken.transfer(msg.sender, balance);
-        usdcToken.transfer(msg.sender, rewardAmountOfSender);
+        if (balance <= 0) {
+            revert("No Staking balance available for the user");
+        }
 
         stakingBalance[msg.sender] = 0;
         rewardAmount[msg.sender] = 0;
         totalStackingBalance -= balance;
 
-        isStaking[msg.sender] = false;
+        hasStaked[msg.sender] = false;
+
+        payable(msg.sender).transfer(balance);
+        usdcToken.transfer(msg.sender, rewardAmountOfSender);
+        emit UnstakedAndRewardsTransferred(
+            msg.sender,
+            balance,
+            rewardAmountOfSender
+        );
     }
 
     function distributeRewards() public {
-        s_vaultState = VaultState.CALCULATING;
         uint totalRewardsAvailable = usdcToken.balanceOf(address(this));
         uint noOfStakers = stakers.length;
 
@@ -92,45 +86,12 @@ contract Vault {
                 totalStackingBalance;
             rewardAmount[stakers[i]] += weightage;
         }
-        s_vaultState = VaultState.OPEN;
 
         // Reset Stakers entry per week
         stakersCountPerWeek = 0;
     }
 
-    function setVaultState(uint vstate) public {
-        s_vaultState = VaultState(vstate);
-    }
-
-    function getRewardAmountOf(address user) public view returns (uint) {
-        return rewardAmount[user];
-    }
-
-    function getVaultETHBalance() public view returns (uint) {
-        return ethToken.balanceOf(address(this));
-    }
-
-    function getEthBalanceOf(address user) public view returns (uint) {
-        return ethToken.balanceOf(user);
-    }
-
-    function getUsdcBalanceOf(address user) public view returns (uint) {
-        return usdcToken.balanceOf(user);
-    }
-
-    function getStakingBalanceOf(address user) public view returns (uint) {
-        return stakingBalance[user];
-    }
-
-    function getEthToken() public view returns (ETHToken) {
-        return ethToken;
-    }
-
-    function getUsdcToken() public view returns (USDCToken) {
-        return usdcToken;
-    }
-
-    function getTotalStakingBalance() public view returns (uint) {
-        return totalStackingBalance;
+    function setLastTimeStampOfRewards(uint _timestamp) public {
+        s_lastTimeStamp = _timestamp;
     }
 }
